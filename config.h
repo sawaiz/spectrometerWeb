e577351b97ec7a834e65eac1baf8abb80e58bdf5
sawaiz <github@sawaizsyed.com>
2017-03-15 22:44:25 -0400

    Added config.h

---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Settings that propogate through the program
+const char* ap_prefix = "esp";
+const char* ap_password = "cosmicSpace";
+const char* http_username = "cosmic";
+const char* http_password = "cosmic";
+
+
+#endif // CONFIG_H
